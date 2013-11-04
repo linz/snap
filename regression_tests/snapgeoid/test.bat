946d80277b26d18aea90ed87f3895794f3040109
Chris Crook <ccrook@linz.govt.nz>
2013-11-04 19:39:31 +1300

    Need explicit extension for local geoid file - to be fixed with file location updates hopefully

--- regression_tests/snapgeoid/test.bat
+++ regression_tests/snapgeoid/test.bat
@@ -2,34 +2,48 @@ SETLOCAL
 
 IF "%SNAPDIR%" == "" SET SNAPDIR=..\..\ms\built\debug
 
+del /s /q out/*
+
 rem basic conversion with and without coordinate system conversion
 
-%SNAPDIR%\snapgeoid -g nzgeoid05 in/test.crd   out/test1.crd
-%SNAPDIR%\snapgeoid -g nzgeoid05 in/test49.crd   out/test2.crd
+echo ================ > out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g nzgeoid05 in/test.crd   out/test1.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g nzgeoid05 in/test49.crd   out/test2.crd >> out/snapgeoid.txt 2>&1
 
 rem different and invalid geoid model
 
-%SNAPDIR%\snapgeoid -g egm96 in/test.crd   out/test3.crd
-%SNAPDIR%\snapgeoid -g garbage in/test.crd   out/test4.crd > out/test4.err
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g egm96nz2k.grd in/test.crd   out/test3.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g garbage in/test.crd   out/test4.crd > out/test4.err >> out/snapgeoid.txt 2>&1
 
 rem updating geoid information
 
-%SNAPDIR%\snapgeoid -g nzgeoid05 out/test3.crd   out/test5.crd
-%SNAPDIR%\snapgeoid -c -g nzgeoid05 out/test3.crd  out/test6.crd
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g nzgeoid05 out/test3.crd   out/test5.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -c -g nzgeoid05 out/test3.crd  out/test6.crd >> out/snapgeoid.txt 2>&1
 
 
 rem removing heights and converting coordinates
 
-%SNAPDIR%\snapgeoid -x -g nzgeoid05 out/test1.crd  out/test7.crd
-%SNAPDIR%\snapgeoid -o -g nzgeoid05 out/test1.crd  out/test8.crd
-%SNAPDIR%\snapgeoid -o -g nzgeoid05 out/test7.crd  out/test8a.crd
-%SNAPDIR%\snapgeoid -e -g nzgeoid05 out/test8.crd  out/test9.crd
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -x -g nzgeoid05 out/test1.crd  out/test7.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -o -g nzgeoid05 out/test1.crd  out/test8.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -o -g nzgeoid05 out/test7.crd  out/test8a.crd >> out/snapgeoid.txt 2>&1
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -e -g nzgeoid05 out/test8.crd  out/test9.crd >> out/snapgeoid.txt 2>&1
 
 rem default geoid
 
-%SNAPDIR%\snapgeoid in/test.crd  out/test10.crd
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid in/test.crd  out/test10.crd >> out/snapgeoid.txt 2>&1
 
 rem out of range coordinates
 
-%SNAPDIR%\snapgeoid -g nzgeoid05 in/test2.crd   out/test11.crd
+echo ================ >> out\snapgeoid.txt 
+%SNAPDIR%\snapgeoid -g nzgeoid05 in/test2.crd   out/test11.crd >> out/snapgeoid.txt 2>&1
 
