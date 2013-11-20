3a043ab6d69433b674c008e739645b007395ce23
Chris Crook <ccrook@linz.govt.nz>
2013-11-21 07:39:24 +1300

    Updating versioninfo.pl

--- src/update_versioninfo.pl
+++ src/update_versioninfo.pl
@@ -6,45 +6,59 @@ use Getopt::Std;
 use POSIX;
 
 my %opts;
-getopts("il",\%opts);
-my $update = $opts{i};
+getopts("iIl",\%opts);
+my $update = $opts{i} || $opts{I};
+my $major = $opts{I};
 my $dolog = $opts{l};
 
 my $nzmapconv = $ARGV[0] eq 'nzmapconv';
 
 my @files;
-find( sub { push(@files,$File::Find::name) if /^versioninfo.c(?:pp)?$/i }, $FindBin::Bin);
+my $root = $FindBin::Bin;
+find( sub { push(@files,$File::Find::name) if /^versioninfo.c(?:pp)?$/i }, $root);
 
 my $buildtime = strftime("%Y-%m-%d %H:%M",localtime);
 
 
-my $log = new FileHandle("$FindBin::Bin/versions.log","a");
-$log->print("\nBuild: $buildtime\n") if $dolog;
+my $log;
+
+if( $dolog )
+{
+    $log = new FileHandle("$FindBin::Bin/versions.log","a");
+    $log->print("\nBuild: $buildtime\n") if $dolog;
+}
 
 foreach my $file (@files)
 {
-	next if $nzmapconv && $file !~ /\/nzmapconv\//i;
-	next if ! $nzmapconv && $file =~ /\/nzmapconv\//i;
+    my $localname=substr($file,length($root));
+    next if $nzmapconv && $file !~ /\/nzmapconv\//i;
+    next if ! $nzmapconv && $file =~ /\/nzmapconv\//i;
 
-	print "Updating version in $file\n";
-	my $fh = new FileHandle("<$file");
-	my $data = join('',$fh->getlines);
-	$fh->close;
-	$data =~ s/^(\s*\#define\s+VERSION\s+\")(\d+\.\d+\.)(\d+)(")/$1.$2.($3+$update).$4/em;
-	my $version = $2.($3+$update);
+    my $fh = new FileHandle("<$file");
+    my $data = join('',$fh->getlines);
+    $fh->close;
+    $data =~ /^\s*\#define\s+VERSION\s+\"(\d+\.\d+\.\d+)\"/m;
+    my $version = $1;
+    print "File $localname version $version\n";
 
-	if( $update )
-	{
-	$fh = new FileHandle(">$file");
-	$fh->print($data);
-	$fh->close;
-	}
+    if( $update )
+    {
+        $version =~ /(\d+)\.(\d+)\.(\d+)/;
+        my ($v1, $v2, $v3) = ($1, $2, $3);
+        if( $major ) { $v2++; $v3=0; } else { $v3++; }
+        $version="$v1.$v2.$v3";
+        print "Updating to $version\n";
+        $data =~ s/^(\s*\#define\s+VERSION\s+\")(\d+\.\d+\.)(\d+)(")/$1.$version.$3/em;
+        $fh = new FileHandle(">$file");
+        $fh->print($data);
+        $fh->close;
+    }
 
-	if( $dolog )
-	{
-	my $program = $1 if $file =~ /\/([^\/]*)\/[^\/]*$/;
-	$log->print("  $program: $version\n");
-	}
+    if( $dolog )
+    {
+        my $program = $1 if $file =~ /\/([^\/]*)\/[^\/]*$/;
+        $log->print("  $program: $version\n");
+    }
 }
-$log->close;
+$log->close if $log;
 
