c4b580e9f577c8f4068674eb805b02c886ef116f
Chris Crook <ccrook@linz.govt.nz>
2015-03-10 15:14:48 +1300

    Beginning of station offset enhancement

--- src/snaplib/snap/deform.h
+++ src/snaplib/snap/deform.h
@@ -1,5 +1,5 @@
-#ifndef _STNOFFSET_H
-#define _STNOFFSET_H
+#ifndef _DEFORM_H
+#define _DEFORM_H
 
 /*
    $Log: deform.h,v $
@@ -9,31 +9,53 @@
 
 */
 
+#ifndef DEFORM_H_RCSID
+#define DEFORM_H_RCSID "$Id: deform.h,v 1.1 1998/06/04 02:24:53 ccrook Exp $"
+#endif
+
 #ifndef _NETWORK_H
 #include "network/network.h"
 #endif
 
-typedef struct
-{
-    double date;
-    double enu[3];
-} stn_tspoint;
-
-typedef struct 
-{
-    int mode;
-    int ordinates;
-    stn_tspoint basepoint;
-    stn_tspoint *tspoints;
-} stn_offset_comp;
+/* Structure for general deformation model */
 
 typedef struct
 {
-    int deformation;
-    int ncomp;
-    stn_offset_comp *components;
+    void *data;
+    int (*init_deformation)( void *model );
+    int (*calc_deformation)( void *model, station *st, double date, double denu[3] );
+    int (*print_model)( void *model, FILE *out, char *prefix );
+    int (*delete_model)( void *model );
+} deformation_model;
 
-} stn_offset;
 
+deformation_model *create_deformation_model(
+    void *data,
+    int (*init_deformation)( void *model ),
+    int (*calc_deformation)( void *model, station *st, double date, double denu[3] ),
+    int (*print_model)( void *model, FILE *out, char *prefix ),
+    int (*delete_model)( void *model )
+);
+
+
+/* Called before the first adjustment iteration to perform any necessary
+   preparation.  All obs and stations are available at this point.
+   Should perform any kind preprocessing/loading, and should check that
+   there will be no problems in terms of extents etc of the model.  */
+
+int init_deformation( deformation_model *deformation );
+
+/* Called for each observation to determine the east, north, and vertical
+   offset that the model predicts for a specific time */
+
+int calc_deformation( deformation_model *deformation, station *st, double date, double denu[3] );
+
+/* Describe the deformation model in an output file */
+
+int print_deformation_model( deformation_model *deformation, FILE *out, char *prefix );
+
+/* Delete the deformation model */
+
+int delete_deformation( deformation_model *deformation );
 
 #endif
