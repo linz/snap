de798344d1e277bb6160b684561680dae7b628e4
Chris Crook <ccrook@linz.govt.nz>
2016-07-27 13:30:50 +1200

    VS2014 projects

---
+++ ms2015/copy_ref_files.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set OPTS=/E /C /I /Y /F
+set SRCDIR=..\src
+set INSTALL=built
+
+for %%v in (Debugx86, Releasex86, Releasex64) do (
+rmdir /S /Q %INSTALL%\%%v\config
+mkdir %INSTALL%\%%v\config
+set SRCDIR=..\src\%version%
+xcopy %OPTS% %SRCDIR%\coordsys\* %INSTALL%\%%v\config\coordsys
+xcopy %OPTS% %SRCDIR%\snap\config\* %INSTALL%\%%v\config
+xcopy %OPTS% %SRCDIR%\snapspec\config\* %INSTALL%\%%v\config
+xcopy %OPTS% %SRCDIR%\snaplist\config\* %INSTALL%\%%v\config
+xcopy %OPTS% %SRCDIR%\snap_manager\config\* %INSTALL%\%%v\config
+)
