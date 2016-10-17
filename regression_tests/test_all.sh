74e9a6f3f2efd4721d3f914e4468d2a26ea6cf2e
Chris Crook <ccrook@linz.govt.nz>
2016-10-17 13:10:04 +1300

    Adding linux test_all script

---
+++ regression_tests/test_all.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+scriptfile=`readlink -f "$0"`
+echo "S0: $0"
+echo "Script file: $scriptfile"
+scriptdir=`dirname $scriptfile`
+
+if [ "$1" == "-r" ]; then
+    export SNAPDIR=${scriptdir}/../unix/release/install
+fi
+
+if [  -z "$SNAPDIR" ]; then
+    export SNAPDIR=${scriptdir}/../unix/debug/install
+fi
+
+for f in `ls -d ${scriptdir}/*/check`; do
+    if [ -d $f ]; then
+        testdir=`dirname $f`
+        testname=`basename $testdir`
+        echo "Testing $testname"
+        if [ -x ${testdir}/test.sh ]; then
+            (cd $testdir && ./test.sh) >> /dev/null 2>&1
+            checkdir=$f
+            if [ -d ${testdir}/check_unix ]; then
+                checkdir=${testdir}/check_unix
+            fi
+            diff -r -b -B -q $checkdir ${testdir}/out
+        else
+            echo "No executable test.sh for $testname"
+        fi
+    fi
+done
