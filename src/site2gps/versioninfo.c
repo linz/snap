efbf8739e31acd428c29e8fde68126f9e42d58fe
Chris Crook <ccrook@linz.govt.nz>
2016-12-19 20:12:59 +1300

    Updating version

--- src/site2gps/versioninfo.c
+++ src/site2gps/versioninfo.c
@@ -1,6 +1,6 @@
 #include "snapconfig.h"
 #define VERSIONINFO
 #define PROGRAM "site2gps"
-#define VERSION "2.7.0"
+#define VERSION "2.7.1"
 
 #include "util/versioninfo.h"
--- src/snaplist/versioninfo.c
+++ src/snaplist/versioninfo.c
@@ -1,6 +1,6 @@
 #include "snapconfig.h"
 #define VERSIONINFO
 #define PROGRAM "listgps"
-#define VERSION "2.7.0"
+#define VERSION "2.7.1"
 
 #include "util/versioninfo.h"
