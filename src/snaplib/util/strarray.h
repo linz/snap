331f106424d77d0fa3f1d0f37e882450a4c3d3b8
Chris Crook <ccrook@linz.govt.nz>
2018-08-14 11:44:52 +1200

    WIP

--- src/snaplib/util/strarray.h
+++ src/snaplib/util/strarray.h
@@ -1,9 +1,7 @@
 #ifndef _STRARRAY_H
 #define _STRARRAY_H
 
-/*
-
-*/
+#include <stdio.h>
 
 typedef struct
 {
@@ -17,9 +15,9 @@ void strarray_init( strarray *stra );
 void strarray_delete( strarray *stra );
 
 int strarray_count( strarray *stra );
-int strarray_find( strarray *stra, char *string );
-int strarray_add( strarray *stra, char *string );
-char *strarray_get( strarray *stra, int i );
+int strarray_find( strarray *stra, const char *string );
+int strarray_add( strarray *stra, const char *string );
+const char *strarray_get( strarray *stra, int i );
 
 void dump_strarray( strarray *stra, FILE *f );
 void reload_strarray( strarray *stra, FILE *f );
