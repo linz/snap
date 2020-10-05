dd32400cd75da14158f07c9e8e2880fe2d645732
Chris Crook <ccrook@linz.govt.nz>
2020-10-05 14:01:21 +1300

    Another try at getting a python environment configured

--- src/packages/linz/findpython.pl
+++ src/packages/linz/findpython.pl
@@ -11,6 +11,7 @@ our $pyprog=$FindBin::Bin.'/testpython.py';
 
 our $testpaths=[
   ['{OSGEO4W}','/bin/python.exe','/apps/Python27'],
+  ['C:/Program Files/QGIS*','/bin/python.exe','/apps/Python27'],
   ['{LOCALAPPDATA}/Microsoft/AppV/Client/Integration/*/Root','/bin/python.exe','/apps/Python27'],
   ['C:/ProgramData/App-V/*/*/Root','/bin/python.exe','/apps/Python27'],
   ['D:/App-V/*/*/Root','/bin/python.exe','/apps/Python27'],
@@ -20,6 +21,8 @@ our $savepath=$ENV{PATH};
 our @needmodules=('osgeo.ogr','psycopg2','numpy');
 our %needed=map {$_=>1} @needmodules;
 our @wantmodules=('PyQt4','scipy');
+our $pypath_template="{pyhome}/Lib;{pyhome}/DLLs;{pyhome}/Lib/plat-win;{pyhome}/Lib/site-packages";
+our $verbose;
 
 sub testpython
 {
@@ -35,6 +38,7 @@ sub testpath
 {
     my($testpath)=@_;
     my($baseglob,$relpyexe,$relpyhome)=@$testpath;
+    print "Testing $baseglob.$relpyexe\n" if $verbose;
     $baseglob=~s/\{(\w+)\}/$ENV{$1}/eg;
     $baseglob=~s/[\\\/]$//;
     my @option=(0,'','','');
@@ -42,6 +46,7 @@ sub testpath
     foreach my $exe (glob('"'.$baseglob.$relpyexe.'"'))
     {
         next if ! -x $exe;
+        print "Found $exe\n" if $verbose;
         my $basepath=substr($exe,0,-length($relpyexe));
         my $pyhome=$basepath.$relpyhome;
         next if ! -d $pyhome;
@@ -52,10 +57,17 @@ sub testpath
         $pyhome=Win32::GetShortPathName($pyhome);
         $pydir =~ s~\/~\\~g;
         $pyhome =~ s~\/~\\~g;
+        my $pypath=$pypath_template;
+        $pypath =~ s/\{pyhome\}/$pyhome/eg;
+        $pypath =~ s/\{pydir\}/$pydir/eg;
         $ENV{PATH} = "$pydir;$pyhome\\scripts;".$savepath;
         $ENV{PYTHONHOME} = $pyhome;
+        $ENV{PYTHONPATH} = $pypath;
+        print "PYDIR: $pydir\nPYHOME $pyhome\nPYPATH $pypath\n" if $verbose;
+
         my ($version, $missing)=testpython($exe);
-        # print("\n$pydir\n",join(" ",$version,@$missing),"\n");
+        my $missingstr=join(" ",@$missing);
+        print "Version $version: missing $missingstr\n" if $verbose;
         my ($status)=2;
         foreach my $m (@$missing)
         {
@@ -63,20 +75,23 @@ sub testpath
             last if $needed{$m};
             $status=1;
         }
-        my $missingstr=join(" ",@$missing);
-        return ($status,$pydir,$pyhome,$version,$missingstr) if $status==2;
+        print "Status: $status\n" if $verbose;
+        return ($status,$pydir,$pyhome,$pypath,$version,$missingstr) if $status==2;
         next if $optversion && version->parse($version) <= $version->parse($optversion);
-        @option=($status,$pydir,$pyhome,$version,$missingstr);
+        @option=($status,$pydir,$pyhome,$pypath,$version,$missingstr);
     }
     return @option;
 }
-my ($pydir,$pyhome,$version,$missing);
+
+$verbose=1 if $ARGV[0] eq '-v';
+my ($pydir,$pyhome,$pypath,$version,$missing);
 foreach my $t (@$testpaths)
 {
-    my ($status,$ppydir,$ppyhome,$pversion,$pmissing)=testpath($t);
+    my ($status,$ppydir,$ppyhome,$ppypath,$pversion,$pmissing)=testpath($t);
+    print "Found: $status $ppydir $ppyhome $ppypath $pversion $pmissing\n" if $verbose;
     next if ! $status;
-    ($pydir,$pyhome,$version,$missing)=($ppydir,$ppyhome,$pversion,$pmissing);
+    ($pydir,$pyhome,$pypath,$version,$missing)=($ppydir,$ppyhome,$ppypath,$pversion,$pmissing);
     last if $status==2;
 }
-print("$version\n$pydir\n$pyhome\n$missing\n") if $pydir;
+print "$version\n$pydir\n$pyhome\n$pypath\n$missing\n" if $pydir || $verbose;
 
