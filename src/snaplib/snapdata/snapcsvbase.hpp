8ddd522123ab407abad9178c481383ef6d5cdc20
Chris Crook <ccrook@linz.govt.nz>
2023-09-28 22:08:32 +1300

    fix: error message for missing columns and optional_columns dtf command
    Note: regressions tests testgx1, testgx2, testgx3 changed due to
    different station having maximum change in last iteration, floating
    point difference?

--- src/snaplib/snapdata/snapcsvbase.hpp
+++ src/snaplib/snapdata/snapcsvbase.hpp
@@ -19,109 +19,125 @@
 
 namespace LINZ
 {
-    namespace SNAP
+namespace SNAP
+{
+
+using namespace LINZ;
+using namespace DelimitedTextFile;
+
+class SnapCsvBase
+{
+public:
+    class CsvValue
     {
+    public:
+        CsvValue(const std::string &name) : _name(name), _column(0) {}
+        ~CsvValue();
+        const std::string &name() {
+            return _name;
+        }
+        void setName(const std::string name);
+        const std::string &value() const {
+            return _column->value();
+        }
+        ConcatValue &cvalue() {
+            return _value;
+        }
+        bool hasValue() {
+            return _value.hasValue();
+        }
+        void attach(CalcReader *reader);
+        template <typename T>
+        bool operator>>(T &value) {
+            return (*_column) >> value;
+        }
 
-        using namespace LINZ;
-        using namespace DelimitedTextFile;
+    private:
+        std::string _name;
+        ConcatValue _value;
+        Column *_column;
+    };
 
-        class SnapCsvBase
-        {
-        public:
-            class CsvValue
-            {
-            public:
-                CsvValue(const std::string &name) : _name(name), _column(0) {}
-                ~CsvValue();
-                const std::string &name() { return _name; }
-                void setName(const std::string name);
-                const std::string &value() const { return _column->value(); }
-                ConcatValue &cvalue() { return _value; }
-                bool hasValue() { return _value.hasValue(); }
-                void attach(CalcReader *reader);
-                template <typename T>
-                bool operator>>(T &value) { return (*_column) >> value; }
+public:
+    SnapCsvBase(const std::string &name, const OptionString &config = OptionString(""));
+    virtual ~SnapCsvBase();
+    bool load(const std::string &filename);
+    bool load(RecordInputBase &input);
+    const std::string &loadedFilename() const {
+        return _loadedFilename;
+    }
+    const std::string &definitionFilename() const {
+        return _definitionFilename;
+    }
 
-            private:
-                std::string _name;
-                ConcatValue _value;
-                Column *_column;
-            };
+protected:
+    // Implementation functions
+    virtual void initiallizeLoadDefinition();
+    virtual void loadDefinitionCommand(const std::string &command, RecordStream &rs) = 0;
+    virtual void terminateLoadDefinition();
 
-        public:
-            SnapCsvBase(const std::string &name, const OptionString &config = OptionString(""));
-            virtual ~SnapCsvBase();
-            void disableColumnCheck(bool check = false) { _checkColumnEnabled = check; }
-            bool load(const std::string &filename);
-            bool load(RecordInputBase &input);
-            const std::string &loadedFilename() const { return _loadedFilename; }
-            const std::string &definitionFilename() const { return _definitionFilename; }
+    virtual void readerAttached() = 0;
+    virtual void initiallizeLoadData();
+    virtual void loadRecord() = 0;
+    virtual void terminateLoadData();
 
-        protected:
-            // Implementation functions
-            virtual void initiallizeLoadDefinition();
-            virtual void loadDefinitionCommand(const std::string &command, RecordStream &rs) = 0;
-            virtual void terminateLoadDefinition();
+    // Helper functions
+    void definitionError(const string &message, RecordErrorType errortype = RuntimeError);
+    void runtimeError(const string &message);
+    void loadValueDefinition(RecordStream &rs, CsvValue &value);
+    void loadValueDefinition(const std::string &valuestr, CsvValue &value);
+    const std::string getConfigValue(const std::string &name, RecordStream &rs);
+    const std::string getConfigValue(const std::string &name, const std::string &valuestr);
 
-            virtual void readerAttached() = 0;
-            virtual void initiallizeLoadData();
-            virtual void loadRecord() = 0;
-            virtual void terminateLoadData();
+    void normaliseColumnNames(CalcReader *reader);
+    void attachReader(CalcReader *reader);
+    bool checkColumns();
 
-            // Helper functions
-            void definitionError(const string &message, RecordErrorType errortype = RuntimeError);
-            void runtimeError(const string &message);
-            void loadValueDefinition(RecordStream &rs, CsvValue &value);
-            void loadValueDefinition(const std::string &valuestr, CsvValue &value);
-            const std::string getConfigValue(const std::string &name, RecordStream &rs);
-            const std::string getConfigValue(const std::string &name, const std::string &valuestr);
+    CalcReader *reader() {
+        return _reader;
+    }
 
-            void normaliseColumnNames(CalcReader *reader);
-            void attachReader(CalcReader *reader);
-            bool checkColumns();
+    LookupMap &lookup(const std::string &lookupName);
 
-            CalcReader *reader() { return _reader; }
+private:
+    void loadDefinition();
+    void loadDefinition(RecordStream &rs);
+    void loadLookupDefinition(RecordStream &rs, const std::string &name, bool skip = false);
+    void loadFormatDefinition(const std::string &formatstr);
+    void loadColumnsDefinition(RecordStream &rs, std::vector<std::string> &cols);
+    void setNSkipLines(int nskip) {
+        _options.setNSkipLines(nskip);
+    }
+    void recordUsedColumn(const std::string &column);
 
-            LookupMap &lookup(const std::string &lookupName);
+    bool _definitionLoaded;
 
-        private:
-            void loadDefinition();
-            void loadDefinition(RecordStream &rs);
-            void loadLookupDefinition(RecordStream &rs, const std::string &name, bool skip = false);
-            void loadFormatDefinition(const std::string &formatstr);
-            void loadColumnsDefinition(RecordStream &rs, std::vector<std::string> &cols);
-            void setNSkipLines(int nskip) { _options.setNSkipLines(nskip); }
-            void recordUsedColumn(const std::string &column);
+    // CSV format options
+    std::string _name;
+    std::string _description;
+    std::string _definitionFilename;
+    std::string _loadedFilename;
+    std::unique_ptr<Format> _format;
+    Options _options;
+    std::vector<std::string> _columns;
+    OptionString _config;
 
-            bool _definitionLoaded;
+    // Columns used in the definition
+    std::vector<std::string> _usedColumns;
+    std::vector<std::string> _requiredColumns;
+    std::vector<std::string> _optionalColumns;
 
-            // CSV format options
-            std::string _name;
-            std::string _description;
-            std::string _definitionFilename;
-            std::string _loadedFilename;
-            std::unique_ptr<Format> _format;
-            Options _options;
-            std::vector<std::string> _columns;
-            OptionString _config;
+    // String lookups
+    std::map<std::string, LookupMap> _lookups;
 
-            bool _checkColumnEnabled;
+    // Currently attached reader
+    CalcReader *_reader;
 
-            // Columns used in the definition
-            std::vector<std::string> _usedColumns;
-            std::vector<std::string> _requiredColumns;
+    // Currently used definition input
+    RecordStream *_defstream;
+};
 
-            // String lookups
-            std::map<std::string, LookupMap> _lookups;
-
-            // Currently attached reader
-            CalcReader *_reader;
-
-            // Currently used definition input
-            RecordStream *_defstream;
-        };
-
-    } // End of namespace SNAP
+} // End of namespace SNAP
 } // End of namespace LINZ
 
 #endif
