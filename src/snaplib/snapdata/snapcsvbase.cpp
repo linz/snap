f408bc37b56637ee2a7c41291ef96f76c131deb6
Chris Crook <ccrook@linz.govt.nz>
2023-09-28 22:08:32 +1300

    fix: error message for missing columns and optional_columns dtf command
    Note: regressions tests testgx1, testgx2, testgx3 changed due to
    different station having maximum change in last iteration, floating
    point difference?

--- src/snaplib/snapdata/snapcsvbase.cpp
+++ src/snaplib/snapdata/snapcsvbase.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/join.hpp>
 #include <iostream>
 #include <memory>
 #include <sstream>
@@ -41,11 +42,10 @@ void SnapCsvBase::CsvValue::attach(CalcReader *reader)
 // SnapCsvBase
 
 SnapCsvBase::SnapCsvBase(const std::string &name, const OptionString &config) : _definitionLoaded(false),
-                                                                                _name(name),
-                                                                                _format(new CsvFormat()),
-                                                                                _config(config),
-                                                                                _checkColumnEnabled(false),
-                                                                                _reader(0)
+    _name(name),
+    _format(new CsvFormat()),
+    _config(config),
+    _reader(0)
 {
 }
 
@@ -112,7 +112,10 @@ void SnapCsvBase::loadDefinition(RecordStream &rs)
         else if (command == "required_columns")
         {
             loadColumnsDefinition(rs, _requiredColumns);
-            disableColumnCheck();
+        }
+        else if (command == "optional_columns")
+        {
+            loadColumnsDefinition(rs, _optionalColumns);
         }
         else if (command == "required_configuration")
         {
@@ -151,7 +154,7 @@ void SnapCsvBase::loadDefinition(RecordStream &rs)
         rs.record() >> command;
         boost::to_lower(command);
 
-        if (command == "format_name" || command == "format" || command == "columns" || command == "required_columns" || command == "required_configuration" || command == "skip_lines")
+        if (command == "format_name" || command == "format" || command == "columns" || command == "required_columns" || command == "optional_columns" || command == "required_configuration" || command == "skip_lines")
         {
             continue;
         }
@@ -177,11 +180,7 @@ bool SnapCsvBase::load(const std::string &filename)
 void SnapCsvBase::attachReader(CalcReader *reader)
 {
     _reader = reader;
-
     readerAttached();
-
-    if (_reader)
-        checkColumns();
 }
 
 bool SnapCsvBase::checkColumns()
@@ -189,22 +188,42 @@ bool SnapCsvBase::checkColumns()
     bool ok = true;
     if (!_reader)
         return ok;
-    std::vector<std::string> *cols = _checkColumnEnabled ? &_usedColumns : &_requiredColumns;
+    std::vector<std::string> *cols = &_requiredColumns;
+    if( cols->size() == 0) cols = &_usedColumns;
 
-    std::vector<std::string> checkedCols;
+    std::vector<std::string> missingCols;
     for (auto col = cols->begin(); col < cols->end(); col++)
     {
-        std::string ucol = *col;
-        boost::to_upper(ucol);
-        if (std::find(checkedCols.begin(), checkedCols.end(), ucol) != checkedCols.end())
+        bool colok=false;
+        for( auto n = missingCols.begin(); n < missingCols.end(); n++ )
         {
-            continue;
+            if( boost::iequals(*col,*n))
+            {
+                colok=true;
+                continue;
+            }
         }
+        if( colok ) continue;
+        for( auto n = _optionalColumns.begin(); n < _optionalColumns.end(); n++ )
+        {
+            if( boost::iequals(*col,*n))
+            {
+                colok=true;
+                continue;
+            }
+        }
+        if( colok ) continue;
         if (!_reader->findColumn(*col))
         {
-            _reader->runtimeError(std::string("Column ") + *col + " is not defined");
-            ok = false;
+            std::string missing=*col;
+            missingCols.push_back(missing);
         }
+    }
+    if( missingCols.size() > 0 )
+    {
+        std::string collist=boost::algorithm::join(missingCols, ", ");
+        _reader->runtimeError(std::string("Column ") + collist + " is not defined");
+        ok = false;
     }
     return ok;
 }
@@ -339,6 +358,8 @@ void SnapCsvBase::loadValueDefinition(const std::string &valuestr, CsvValue &val
         RGX::regex_constants::icase);
     RGX::sregex_iterator imatch(valuestr.begin(), valuestr.end(), re);
     RGX::sregex_iterator end;
+    std::vector<std::string> usedColumns;
+    bool haveDefault=false;
     for (; imatch != end; imatch++)
     {
         auto match = *imatch;
@@ -348,6 +369,7 @@ void SnapCsvBase::loadValueDefinition(const std::string &valuestr, CsvValue &val
             ConcatValue *owner = cvalue;
             cvalue = new ConcatValue();
             owner->setDefault(cvalue);
+            haveDefault=true;
         }
         // Numeric value
         else if (match[2].length() > 0)
@@ -375,7 +397,7 @@ void SnapCsvBase::loadValueDefinition(const std::string &valuestr, CsvValue &val
                 definitionError(std::string("Column name ") + match[6].str() + " for " + value.name() + "should start with @", Warning);
             }
             std::string colname = match[5].str() + match[6].str();
-            recordUsedColumn(colname);
+            usedColumns.push_back(colname);
             NamedValue col(colname, "");
             cvalue->add(col);
         }
@@ -406,7 +428,7 @@ void SnapCsvBase::loadValueDefinition(const std::string &valuestr, CsvValue &val
                 LookupValue *lv = new LookupValue();
                 lv->setMap(map);
                 std::string colname = match[9].str() + match[10].str();
-                recordUsedColumn(colname);
+                usedColumns.push_back(colname);
                 lv->setSource(new NamedValue(colname, ""));
                 cvalue->add(lv);
             }
@@ -421,6 +443,17 @@ void SnapCsvBase::loadValueDefinition(const std::string &valuestr, CsvValue &val
             break;
         }
     }
+
+    // Record the used columns if there is no default value.  If there is one or more defaults
+    // then assume that the column is not required.
+    if( ! haveDefault )
+    {
+        for (auto c = usedColumns.begin(); c < usedColumns.end(); c++)
+        {
+            recordUsedColumn(*c);
+        }
+    }
+
     if (!value.cvalue().hasValue())
     {
         definitionError(string("Value not defined for ") + value.name());
