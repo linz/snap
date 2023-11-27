85ee5fb4b186c9cbe9a0abd6051a02129e1561b8
Chris Crook <ccrook@linz.govt.nz>
2023-11-27 20:38:28 +1300

    feat: updating Windows build to wxWidgets-3.2.4.7z

--- wxwidgets/build_wx.bat
+++
@@ -1,9 +0,0 @@
-REM Build wx widgets libraries
-
-cd wxwidgets\build\msw
-nmake -f makefile.vc BUILD=debug SHARED=0 RUNTIME_LIBS=static clean >NULL
-nmake -f makefile.vc BUILD=release SHARED=0 RUNTIME_LIBS=static clean >NULL
-echo Building wxwidgets 32bit debug version
-nmake -f makefile.vc BUILD=debug SHARED=0 RUNTIME_LIBS=static  > build_wxd.log 2>&1
-echo Building wxwidgets 32bit release version
-nmake -f makefile.vc BUILD=release SHARED=0 RUNTIME_LIBS=static  > build_wx.log 2>&1
